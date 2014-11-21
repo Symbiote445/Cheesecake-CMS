13f226becc8afe36e8206d418ef052374c79e4df
Symbiote445 <gage@ccp2.us>
2014-11-21 00:11:28 -0500

    Gallery!
    Added way better visuals to the gallery

--- include/scripts/gallery.class.php
+++ include/scripts/gallery.class.php
@@ -1,4 +1,236 @@
 	<style>
+.og-grid {
+	list-style: none;
+	padding: 20px 0;
+	margin: 0 auto;
+	text-align: center;
+	width: 100%;
+}
+
+.og-grid li {
+	display: inline-block;
+	margin: 10px 5px 0 5px;
+	vertical-align: top;
+	height: 250px;
+}
+
+.og-grid li > a,
+.og-grid li > a img {
+	border: none;
+	outline: none;
+	display: block;
+	position: relative;
+}
+
+.og-grid li.og-expanded > a::after {
+	top: auto;
+	border: solid transparent;
+	content: " ";
+	height: 0;
+	width: 0;
+	position: absolute;
+	pointer-events: none;
+	border-bottom-color: #ddd;
+	border-width: 15px;
+	left: 50%;
+	margin: -20px 0 0 -15px;
+}
+
+.og-expander {
+	position: absolute;
+	background: #ddd;
+	top: auto;
+	left: 0;
+	width: 100%;
+	margin-top: 10px;
+	text-align: left;
+	height: 0;
+	overflow: hidden;
+}
+
+.og-expander-inner {
+	padding: 50px 30px;
+	height: 100%;
+}
+
+.og-close {
+	position: absolute;
+	width: 40px;
+	height: 40px;
+	top: 20px;
+	right: 20px;
+	cursor: pointer;
+}
+
+.og-close::before,
+.og-close::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	top: 50%;
+	height: 1px;
+	background: #888;
+	-webkit-transform: rotate(45deg);
+	-moz-transform: rotate(45deg);
+	transform: rotate(45deg);
+}
+
+.og-close::after {
+	-webkit-transform: rotate(-45deg);
+	-moz-transform: rotate(-45deg);
+	transform: rotate(-45deg);
+}
+
+.og-close:hover::before,
+.og-close:hover::after {
+	background: #333;
+}
+
+.og-fullimg,
+.og-details {
+	width: 50%;
+	float: left;
+	height: 100%;
+	overflow: hidden;
+	position: relative;
+}
+
+.og-details {
+	padding: 0 40px 0 20px;
+}
+
+.og-fullimg {
+	text-align: center;
+}
+
+.og-fullimg img {
+	display: inline-block;
+	max-height: 100%;
+	max-width: 100%;
+}
+
+.og-details h3 {
+	font-weight: 300;
+	font-size: 52px;
+	padding: 40px 0 10px;
+	margin-bottom: 10px;
+}
+
+.og-details p {
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 22px;
+	color: #999;
+}
+
+.og-details a {
+	font-weight: 700;
+	font-size: 16px;
+	color: #333;
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	padding: 10px 20px;
+	border: 3px solid #333;
+	display: inline-block;
+	margin: 30px 0 0;
+	outline: none;
+}
+
+.og-details a::before {
+	content: '\2192';
+	display: inline-block;
+	margin-right: 10px;
+}
+
+.og-details a:hover {
+	border-color: #999;
+	color: #999;
+}
+
+.og-loading {
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background: #ddd;
+	box-shadow: 0 0 1px #ccc, 15px 30px 1px #ccc, -15px 30px 1px #ccc;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	margin: -25px 0 0 -25px;
+	-webkit-animation: loader 0.5s infinite ease-in-out both;
+	-moz-animation: loader 0.5s infinite ease-in-out both;
+	animation: loader 0.5s infinite ease-in-out both;
+}
+
+@-webkit-keyframes loader {
+	0% { background: #ddd; }
+	33% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ccc, -15px 30px 1px #ddd; }
+	66% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ddd, -15px 30px 1px #ccc; }
+}
+
+@-moz-keyframes loader {
+	0% { background: #ddd; }
+	33% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ccc, -15px 30px 1px #ddd; }
+	66% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ddd, -15px 30px 1px #ccc; }
+}
+
+@keyframes loader {
+	0% { background: #ddd; }
+	33% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ccc, -15px 30px 1px #ddd; }
+	66% { background: #ccc; box-shadow: 0 0 1px #ccc, 15px 30px 1px #ddd, -15px 30px 1px #ccc; }
+}
+
+@media screen and (max-width: 830px) {
+
+	.og-expander h3 { font-size: 32px; }
+	.og-expander p { font-size: 13px; }
+	.og-expander a { font-size: 12px; }
+
+}
+
+@media screen and (max-width: 650px) {
+
+	.og-fullimg { display: none; }
+	.og-details { float: none; width: 100%; }
+	
+}
+.main > p {
+	text-align: center;
+	padding: 50px 20px;
+}
+
+/* Header Style */
+.codrops-top {
+	line-height: 24px;
+	font-size: 11px;
+	background: #fff;
+	background: rgba(255, 255, 255, 0.5);
+	text-transform: uppercase;
+	z-index: 9999;
+	position: relative;
+	box-shadow: 1px 0px 2px rgba(0,0,0,0.2);
+}
+
+.codrops-top a {
+	padding: 0px 10px;
+	letter-spacing: 1px;
+	color: #333;
+	display: inline-block;
+}
+
+.codrops-top a:hover {
+	background: rgba(255,255,255,0.8);
+	color: #000;
+}
+
+.codrops-top span.right {
+	float: right;
+}
+
+.codrops-top span.right a {
+	float: left;
+	display: block;
+}
 .imgHolder { 
 	display:none;
     position: absolute;
@@ -16,6 +248,7 @@ overflow:hidden;
 
 .close { padding:5px; }
 	</style>
+	<!--
 	<script type="text/javascript">
 var observe;
 if (window.attachEvent) {
@@ -60,6 +293,26 @@ $(document).ready(function(){
 });
 
 </script>
+-->
+
+<script src="include/scripts/js/modernizr.custom.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+     $(document).ready(function(){
+           $('li img').on('click',function(){
+                var src = $(this).attr('src');
+                var img = '<img src="' + src + '" class="img-responsive"/>';
+                $('#myModal').modal();
+                $('#myModal').on('shown.bs.modal', function(){
+                    $('#myModal .modal-body').html(img);
+                });
+                $('#myModal').on('hidden.bs.modal', function(){
+                    $('#myModal .modal-body').html('');
+                });
+           });  
+        })
+</script>
 
 <?php
 /*
@@ -77,39 +330,41 @@ if(isset($_GET['action'])){
 class gallery{
 	function vg(){
 		global $dbc, $parser, $layout, $main, $settings, $core;
-		echo'<div class="shadowbar">
-			<div class="imgHolder" style="display:none;">
-			<div class="close">X</div>
-				<img style="max-height:500px" src="" /> 
-			</div>';
-		echo'<h3>Gallery</h3>';
 		if($core->verify("4") || $core->verify("2")){
-		echo '<a class="Link LButton" href="index.php?action=uploadphoto">Upload File To Gallery </a><br>';
+		echo '<div class="shadowbar"><a class="Link LButton" href="index.php?action=uploadphoto">Upload File To Gallery </a></div>';
 		}
+		echo'<div class="shadowbar">';
+		echo'<h3>Gallery</h3>';
 		if(isset($_GET['limit'])){
 		$secureLimit = preg_replace("/[^0-9]/", "", $_GET['limit']);
 		$limit = mysqli_real_escape_string($dbc, $secureLimit);
 		} else {
 		$limit = 0;
 		}
-		$query = "SELECT * FROM gallery ORDER BY p_id LIMIT $limit,1";
+		$query = "SELECT * FROM gallery ORDER BY p_id LIMIT $limit,10";
 		mysqli_query($dbc, $query);
 		$data = mysqli_query($dbc, $query);
+		echo'
+			<div class="main">
+				<ul id="og-grid" class="og-grid">';
 		while($row = mysqli_fetch_array($data)){
-		echo '<div class="gci"> ';
 			if(!empty($row['filename'])){
-				echo'<img class="city" style="max-height:100px" data-img="include/images/'.$row['filename'].'" src="'.MM_GALLERY, $row['filename'].'"><br />';
-			}
+				echo'<li>
+						<a href="http://'.$settings['b_url'].'/index.php?action=viewgallery" data-largesrc="include/images/'.$row['filename'].'" data-title="'.$row['name'].'" data-description="'.$row['descr'].'">
+							<img style="max-height:140px;" src="include/images/'.$row['filename'].'" alt="img01"/>
+						</a>
+					</li>';
+			} /*
 			if(!empty($row['name'])){
-				echo '<strong>'.$row['name'] . '</strong><br />';
+				echo '<strong>'.$row['name'] . '</strong>';
 			}
 			if(!empty($row['descr'])){
 				echo $row['descr'];
-			}
-			
-			echo '</div>';
+			}*/
 		}
-		echo '<a class="Link LButton" href="index.php?action=viewgallery&limit='.($limit - 1).'">Previous </a><a class="Link LButton" href="index.php?action=viewgallery&limit='.($limit + 1).'"> Next</a>';
+		echo '</ul>
+				</div>';
+		echo '<a class="Link LButton" href="index.php?action=viewgallery&limit='.($limit - 10).'">Previous </a><a class="Link LButton" href="index.php?action=viewgallery&limit='.($limit + 10).'"> Next</a>';
 		echo '</div>';
 	}
 	
@@ -182,3 +437,9 @@ class gallery{
 	}
 
 ?>
+<script src="include/scripts/js/grid.js"></script>
+		<script>
+			$(function() {
+				Grid.init();
+			});
+		</script>
