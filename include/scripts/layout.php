8cbae80e50d023aff8b86c939b2027190e0f014f
Symbiote445 <gage@ccp2.us>
2014-11-20 22:09:37 -0500

    Navbar
    More friendly navbar - however the header logo was removed.

--- include/scripts/layout.php
+++ include/scripts/layout.php
@@ -21,29 +21,35 @@ $layout = array(
 		<body>
 			<div class="row header">
 				<div class="col-md-1"></div>
-				<div class="col-md-3 logo">
-					<img src="include/images/logo.png">
-				</div>
-				<div class="col-md-7">
-					<div class="navbar-start">
-						<div class="navbar-container">
-							<nav class="navbar-inverse pad5" role="navigation">
-								<div class="navbar-header">
-									 <a href="index.php" class="navbar-brand">%s</a>
-									</div>
-								  <ul class="nav navbar-nav">
-										<li><a href="index.php">Home</a></li>
+   <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">%s</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
 										
 		',
 	'header-end'=>'
-								  </ul>
-							</nav>
-						</div>
-					</div>
-				</div>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
 				<div class="col-md-1"></div>
 			</div>
-			<div class="grayout" style="display:none;"></div>
 			<div class="row content">	
 			',
 	'blogViewFormat'=>'
